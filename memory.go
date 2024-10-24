7a1a4185d862fa084ee2c3100c75bff2bde73c7b
Saheed Adeleye <aadesaed@gmail.com>
2024-10-24 01:25:43 +0100

    memory: memory.go currently only stores some constants
    it'll probably go away, maybe?

---
+++ memory.go
@@ -0,0 +1,8 @@
+package main
+
+const (
+	MEMORY_SIZE            int    = 2048
+	RETSTACK_SIZE          int    = 2048
+	PROGRAM_ADDRESS_OFFSET int    = 0x0200
+	FONT_DATA_BASE_ADDRESS uint16 = 0x50
+)
